6cbdcff89003cc97dc2595a154f99fad82082caf
Mina Eskander <127833856+Mina777-0@users.noreply.github.com>
2024-06-21 17:25:25 +0300

    Update urls.py

--- users/urls.py
+++ users/urls.py
@@ -9,4 +9,6 @@ urlpatterns= [
     path('api_signin', views.api_signin, name= "api_signin"),
     path('insurance_policy', views.insurance_policy, name="insurance_policy"),
     path('get_policies', views.get_policies, name="get_policies"),
+    path('add_family_member', views.add_family_member, name="add_family_member"),
+    path('get_family_members', views.get_family_members, name="get_family_members"),
 ]
